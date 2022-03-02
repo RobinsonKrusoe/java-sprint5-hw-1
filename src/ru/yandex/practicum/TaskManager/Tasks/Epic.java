cfa237334eb57f5d5a4fd91d882b0684a1dd02b7
mmm <RobinsonKrusoe@yandex.ru>
2022-03-02 22:22:49 +0300

    Спринт #5

---
+++ src/ru/yandex/practicum/TaskManager/Tasks/Epic.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.TaskManager.Tasks;
+
+import ru.yandex.practicum.TaskManager.Tasks.BaseTask.*;
+
+import java.util.ArrayList;
+
+//Класс Эпика (Задачи с Подзадачами)
+public class Epic extends Task {
+    private ArrayList<SubTask> subTasks;    //Список подзадач
+
+    //Конструктор
+    public Epic(Integer num, String name, String details) {
+        super(num, name, details, TaskType.EPIC);
+
+        subTasks = new ArrayList<>();
+    }
+
+    //Получение статуса Эпика
+    public TaskStatus getStatus(){
+        TaskStatus result;
+        int statusSum = TaskStatus.NEW.ordinal();
+
+        if (subTasks.size() == 0){  //Если нет подзадач
+            result = TaskStatus.NEW;  //Для пустого эпика вернуть статус NEW
+        } else {  //Статус непустых эпиков определяется статусами подзадач
+            for (SubTask subTask : subTasks)
+                statusSum += subTask.getStatus().ordinal();
+
+            if (statusSum == TaskStatus.NEW.ordinal()){ //Все подзадачи новые
+                result = TaskStatus.NEW;
+            } else if (statusSum == (TaskStatus.DONE.ordinal() * subTasks.size())){  //Все подзадачи выполнены
+                result = TaskStatus.DONE;
+            } else {
+                result = TaskStatus.IN_PROGRESS;
+            }
+        }
+
+        return result;
+    }
+
+    //Получение списка всех подзадач
+    public ArrayList<SubTask> getSubTasks() {
+        return subTasks;
+    }
+
+    //Отображение Эпика
+    @Override
+    public String toString() {
+        return getNum() + "," + getType() + "," + getName() + "," + getStatus() + "," + getDetails() + ",";
+    }
+}
---
+++ src/ru/yandex/practicum/TaskManager/Tasks/SubTask.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.TaskManager.Tasks;
+
+import ru.yandex.practicum.TaskManager.Tasks.BaseTask.Task;
+import ru.yandex.practicum.TaskManager.Tasks.BaseTask.TaskType;
+
+//Класс подзадачи
+public class SubTask extends Task {
+    Epic epic;    //Ссылка на родительский Эпик
+
+    //Конструктор
+    public SubTask(Integer num, String name, String details, Epic epic) {
+        super(num, name, details, TaskType.SUBTASK);
+
+        this.epic = epic;
+    }
+
+    public SubTask(String name, String details, Epic epic) {
+        super(name, details, TaskType.SUBTASK);
+
+        this.epic = epic;
+    }
+
+    //Получение Эпика
+    public Epic getEpic() {
+        return epic;
+    }
+
+    //Задание Эпика
+    public void setEpic(Epic epic) {
+        this.epic = epic;
+    }
+
+    //Отображение задачи
+    @Override
+    public String toString() {
+        return getNum() + "," + getType() + "," + getName() + "," + getStatus() + "," + getDetails() + "," + getEpic().getNum();
+    }
+}
